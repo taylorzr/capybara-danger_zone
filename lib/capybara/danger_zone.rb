37e354806ef36365ff10e8301b28f1b09bf641be
Zach Taylor <taylorzr@gmail.com>
2016-10-13 22:50:46 -0500

    Add capybara and basic configuration

--- lib/capybara/danger_zone.rb
+++ lib/capybara/danger_zone.rb
@@ -1,7 +1,32 @@
+require 'capybara'
 require 'capybara/danger_zone/version'
 
 module Capybara
   module DangerZone
-    # Your code goes here...
+    DangerZoneError = Class.new(StandardError)
+
+    Capybara::Node::Base.prepend(Highway)
+
+    class << self
+      def enabled?
+        @enabled
+      end
+
+      def enable
+        @enabled = true
+      end
+
+      def disable
+        @enabled = false
+      end
+
+      def rate_threshold
+        @rate_threshold ||= 0.8
+      end
+
+      def rate_threshold=(value)
+        @rate_threshold = value
+      end
+    end
   end
 end
